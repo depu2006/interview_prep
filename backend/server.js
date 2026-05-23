95682d41c7241748e07e5c5bdb3e61e55eaab4bf
Shree <deepshika@gmail.com>
2026-05-23 10:59:33 +0530

    Update CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,7 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
-  credentials: true,
+  origin: '*'
 }))
 app.use(express.json())
 
